22aff67a611ec8c974fb2e84167e20ad6b265191
sotanmochi <16936357+sotanmochi@users.noreply.github.com>
2023-09-16 17:19:20 +0900

    feat: Update SignalType

--- src/SignalStreaming.Infrastructure/SignalType.cs
+++ src/SignalStreaming.Infrastructure/SignalType.cs
@@ -10,12 +10,17 @@ namespace Crossoverse.SignalStreaming.Infrastructure
 
         // Buffered
         CreateObject = BufferedSignal + 1,
+        ItemSpawn = BufferedSignal + 2,
+        PlayerSpawn = BufferedSignal + 3,
 
         // Low Frequency
         TextMessage = LowFreqSignal + 1,
         DestroyObject = LowFreqSignal + 2,
+        ItemDespawn = BufferedSignal + 3,
+        PlayerDespawn = BufferedSignal + 4,
 
         // High Frequency
         ObjectPose = HighFreqSignal + 1,
+        ItemPose = BufferedSignal + 2,
     }
 }
